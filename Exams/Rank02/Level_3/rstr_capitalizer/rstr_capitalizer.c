6871077e37d08e46eb76ac37c184cd746d65b419
neusamcn <64598994+neusamcn@users.noreply.github.com>
2025-07-21 19:57:46 +0100

    Done

---
+++ Exams/Rank02/Level_3/rstr_capitalizer/rstr_capitalizer.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+
+int main(int ac, char **av)
+{
+    int row = 1;
+    int col;
+    if (ac > 1)
+    {
+        while (row < ac)
+        {
+            col = 0;
+            while (av[row][col] && av[row][col] != '\0')
+            {
+                // Capitalizing last character of words:   
+                if (av[row][col + 1] == '\0' || av[row][col + 1] == ' ' ||
+                    (av[row][col + 1] >= '\t' && av[row][col + 1] <= '\r'))
+                {
+                    if (av[row][col] >= 'a' && av[row][col] <= 'z')
+                        av[row][col] = av[row][col] - 32;
+                }
+                // Uppercases that need to become lowercase:
+                else
+                {
+                    if (av[row][col] >= 'A' && av[row][col] <= 'Z')
+                        av[row][col] = av[row][col] + 32;
+                }
+                // write() everything:
+                write(1, &av[row][col], 1);
+                col++;
+            }
+            write(1, "\n", 1);
+            row++;
+        }
+    }
+    else
+        write(1, "\n", 1);
+    return (0);
+}
