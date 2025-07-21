fe4f1664fed0f0c4eebc70bc9abb134437a6d4d0
neusamcn <64598994+neusamcn@users.noreply.github.com>
2025-07-21 19:48:05 +0100

    Done
    More readable code

--- Exams/Rank02/Level_3/str_capitalizer/str_capitalizer.c
+++ Exams/Rank02/Level_3/str_capitalizer/str_capitalizer.c
@@ -12,15 +12,18 @@ int main(int ac, char **av)
             while (av[row][col] && av[row][col] != '\0')
             {
                 // Capitalizing words:   
-                if ((av[row][col] >= 'a' && av[row][col] <= 'z') &&
-                    (col == 0 || av[row][col - 1] == ' ' ||
-                    (av[row][col - 1] >= '\t' && av[row][col - 1] <= '\r')))
+                if (col == 0 || av[row][col - 1] == ' ' ||
+                    (av[row][col - 1] >= '\t' && av[row][col - 1] <= '\r'))
+                {
+                    if (av[row][col] >= 'a' && av[row][col] <= 'z')
                         av[row][col] = av[row][col] - 32;
+                }
                 // Uppercases that need to become lowercase:
-                else if ((av[row][col] >= 'A' && av[row][col] <= 'Z') &&
-                !((col == 0 || av[row][col - 1] == ' ' ||
-                (av[row][col - 1] >= '\t' && av[row][col - 1] <= '\r'))))
+                else
+                {
+                    if (av[row][col] >= 'A' && av[row][col] <= 'Z')
                         av[row][col] = av[row][col] + 32;
+                }
                 // write() everything:
                 write(1, &av[row][col], 1);
                 col++;
