afe818a2dfe3d8f1c57098f5c7b64e17fe79b7be
Neusa Muhala Cruz Neto <ncruz-ne@c2r8s7.42lisboa.com>
2025-07-25 17:36:12 +0100

    sort_int_tab.c Done

--- Exams/Rank02/Level_4/sort_int_tab/sort_int_tab.c
+++ Exams/Rank02/Level_4/sort_int_tab/sort_int_tab.c
@@ -6,16 +6,43 @@
 /*   By: ncruz-ne <ncruz-ne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/24 20:36:01 by ncruz-ne          #+#    #+#             */
-/*   Updated: 2025/07/24 20:52:12 by ncruz-ne         ###   ########.fr       */
+/*   Updated: 2025/07/25 17:35:44 by ncruz-ne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void sort_int_tab(int *tab, unsigned int size)
+void	sort_int_tab(int *tab, unsigned int size)
 {
-    int i = 0;
+	unsigned int	i;
+	int	temp;
 
-    while (i < size)
-    {
-        tab[i]
-    }
+	i = 0;
+	while (i < size - 1)
+	{
+		if (tab[i] > tab[i + 1])
+		{
+			temp = tab[i];
+			tab[i] = tab[i + 1];
+			tab[i + 1] = temp;
+            i = 0;
+            continue;
+		}
+		i++;
+	}
 }
+// // FOR TESTING ONLY:
+// #include <stdio.h>
+
+// int	main()
+// {
+// 	int tab[] = {5, 2, 2, 1};
+// 	unsigned int size = 4;
+// 	unsigned int i = 0;
+
+//     sort_int_tab(tab, size);
+// 	while (i < size)
+// 	{
+// 		printf("%d\n", tab[i]);
+// 		i++;
+// 	}
+// 	return (0);
+// }
