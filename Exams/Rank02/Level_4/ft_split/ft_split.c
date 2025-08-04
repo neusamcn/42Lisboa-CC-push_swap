b9a5da381dc8966041043d505601c16aea3dc7a6
Neusa Muhala Cruz Neto <ncruz-ne@c2r9s6.42lisboa.com>
2025-08-04 16:19:12 +0100

    Level 4

---
+++ Exams/Rank02/Level_4/ft_split/ft_split.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ncruz-ne <ncruz-ne@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/04 14:51:17 by ncruz-ne          #+#    #+#             */
+/*   Updated: 2025/08/04 14:56:48 by ncruz-ne         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+
+char    **ft_split(char *str)
+{
+    char    **splat;
+    // Allocating memory to the array of strings result:
+    splat = 
+    while ()
+    {
+        
+    }
+    return (splat);
+}
