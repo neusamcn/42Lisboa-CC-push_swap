cd75725d378e3c45714d11b49a607af0235c078e
neusamcn <neusaneto@gmail.com>
2025-08-06 21:35:29 +0100

    ft_split.c Done

--- Exams/Rank02/Level_4/ft_split/ft_split.c
+++ Exams/Rank02/Level_4/ft_split/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: neusa <neusa@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/04 14:51:17 by ncruz-ne          #+#    #+#             */
-/*   Updated: 2025/08/06 20:26:05 by neusa            ###   ########.fr       */
+/*   Updated: 2025/08/06 21:33:48 by neusa            ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -14,7 +14,9 @@
 
 size_t	whitespace_skips(char *str)
 {
-	size_t	i = 0;
+	size_t	i;
+
+	i = 0;
 	// not considering form feed ('\f') or carriage return ('\r') to skip:
 	while (str[i] == ' ' || (str[i] >= '\t' && str[i] <= '\v'))
 		i++;
@@ -41,13 +43,14 @@ size_t	count_rows(char *str)
 	return (rows);
 }
 
-size_t  ft_row_len(char *str)
+size_t	ft_row_len(char *str)
 {
-    size_t  row_len = 0;
-    
-    while (str[row_len] != '\0' && whitespace_skips(&str[row_len]) == 0)
-        row_len++;
-    return (row_len);
+	size_t	row_len;
+
+	row_len = 0;
+	while (str[row_len] != '\0' && whitespace_skips(&str[row_len]) == 0)
+		row_len++;
+	return (row_len);
 }
 
 void	free_all(char **splat, size_t row)
@@ -64,45 +67,48 @@ char	**ft_split(char *str)
 {
 	size_t	i;
 	size_t	row;
-	size_t	total_rows = count_rows(str);
-    size_t  row_len;
+	size_t	total_rows;
+	size_t	row_len;
 	size_t	col;
 	char	**splat;
 
 	if (!str)
 		return (NULL);
 	// Allocating memory for the array of pointers:
+	total_rows = count_rows(str);
 	splat = (char **)malloc(sizeof(char *) * (total_rows + 1));
 	if (!splat)
 		return (NULL);
 	// Splitting str:
 	i = 0;
 	row = 0;
-    while (str[i] != '\0' && row < total_rows)
-    {
-        // Skipping spaces, tabs and new lines:
-        i += whitespace_skips(&str[i]);
-        // Calculating length of a row:
-        row_len = ft_row_len(&str[i]);
-        // Allocating memory for each row of splat:
-        splat[row] = (char *)malloc(sizeof(char) * (row_len + 1));
-        if (!splat[row])
+	while (str[i] != '\0' && row < total_rows)
+	{
+		// Skipping spaces, tabs and new lines:
+		i += whitespace_skips(&str[i]);
+		// Check if we've reached end of string:
+		if (str[i] == '\0')
+			break ;
+		// Calculating length of a row:
+		row_len = ft_row_len(&str[i]);
+		// Allocating memory for each row of splat:
+		splat[row] = (char *)malloc(sizeof(char) * (row_len + 1));
+		if (!splat[row])
 		{
 			free_all(splat, row);
 			return (NULL);
 		}
-        row_len = 0;
-        // Create a new splat[row]:
-        col = 0;
-        while (str[i] != '\0' && whitespace_skips(&str[i]) == 0)
-        {
-            splat[row][col] = str[i];
-            i++;
-            col++;
-        }
-        splat[row][col] = '\0';
-        row++;
-    }
+		// Create a new splat[row]:
+		col = 0;
+		while (str[i] != '\0' && whitespace_skips(&str[i]) == 0)
+		{
+			splat[row][col] = str[i];
+			i++;
+			col++;
+		}
+		splat[row][col] = '\0';
+		row++;
+	}
 	// Last row of splat:
 	splat[row] = NULL;
 	return (splat);
