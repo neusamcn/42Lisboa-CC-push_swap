ecab5a680be1089d7caebba097dd95f5f93a3ffd
neusamcn <neusaneto@gmail.com>
2025-08-24 23:04:21 +0100

    setup

--- push-swap/push_swap.c
+++ push-swap/push_swap.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/******************************************************************************/
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   push_swap.c                                        :+:      :+:    :+:   */
@@ -6,17 +6,44 @@
 /*   By: ncruz-ne <ncruz-ne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/11 15:54:12 by ncruz-ne          #+#    #+#             */
-/*   Updated: 2025/08/12 17:22:58 by ncruz-ne         ###   ########.fr       */
+/*   Updated: 2025/08/24 22:52:40 by ncruz-ne         ###   ########.fr       */
 /*                                                                            */
-/* ************************************************************************** */
+/******************************************************************************/
 
 #include "push_swap.h"
+#include <stdio.h>
+int main()
+{
 
-
+}
 
 /* TO-DO:
 - STACK STRUCT:
 -- len : size of the array;
 -- max : highest value;
 -- min : lowest value; 
+
+LOCICAL STEPS:
+1. normalize input data (add left 0s)
+2. separate negatives from positives - if(a[i] < 0){pb;}
+    > stacks a (pos) && b (neg) to join w/ pa() after sort
+(maybe) quick sort up to N items (due to speed)
+3. radix sort on a && b - bucket by lsd
+3.1. inverted sort on negative stack - if(sort(a){+>-}){sort(b){->+}}
+6. join positive stack to negative stack - while(b){pa++;}
+
+BUILD:
+sa: swap a
+sb: swap b
+ss: sa + sb
+pa: push a
+pb
+ra: rotate a
+rb
+rr: ra + rb
+rra: reverse rotate a
+rrb
+rrr: rra + rrb
+
+buckets: 
 */
