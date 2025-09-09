29aef42b5e5bb8abdd7a28facc05119462563451
neusamcn <neusaneto@gmail.com>
2025-09-10 00:40:21 +0100

    small cleanup

--- push-swap/find_stack_members.c
+++ push-swap/find_stack_members.c
@@ -6,7 +6,7 @@
 /*   By: ncruz-ne <ncruz-ne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/07 18:17:39 by ncruz-ne          #+#    #+#             */
-/*   Updated: 2025/09/10 00:20:47 by ncruz-ne         ###   ########.fr       */
+/*   Updated: 2025/09/10 00:39:33 by ncruz-ne         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -54,13 +54,6 @@ int	find_radix_max_div(t_stack *stack)
 	return (radix_max_div);
 }
 
-// function for ranks, inversions and stack's min, max and sorted
-// rank?????
-// if (content == min) {rank == 0};
-// if (content == max) {rank == stack->size - 1};
-// inversions = rank - index; goal is 0.
-// sorted = SUM(inversions). goal is 0.
-
 int	count_node_inversions(t_stack *stack, t_circlist *node)
 {
 	int			inversions;
@@ -99,63 +92,3 @@ void	count_stack_inversions(t_stack *stack)
 			break ;
 	}
 }
-
-// bucket_sort + radix_sort ?
-// REQUIRES TESTING:
-// void	ft_mk_lst_rank(t_stack *stack)
-// {
-// 	t_nodes	nodes;
-// 	int		lsd;
-// 	int		bucket;
-// 	int		div;
-
-// 	nodes.previous = NULL;
-// 	nodes.current = NULL;
-// 	div = find_radix_max_div(stack);
-// 	while (bucket >= -9)
-// 	{
-// 		bucket = 9;
-// 		while (stack)
-// 		{
-// 			lsd = (stack->head->content) % div;
-// 			if (lsd == bucket)
-// 			{
-// 			}
-// 			stack->head = stack->head->next;
-// 			if (stack->head->index == 0)
-// 				break ;
-// 		}
-// 		bucket--;
-// 	}
-// }
-
-// quick_sort()
-// Hoare's partition
-// https://www.geeksforgeeks.org/dsa/hoare-s-partition-algorithm/
-// REQUIRES TESTING:
-// t_stack	*hoares_partition(t_stack *stack)
-// {
-// 	int			pivot;
-// 	int			swap;
-// 	t_circlist	*head_current;
-// 	t_circlist	*tail_current;
-// 	// t_stack		*cp_stack;
-
-// 	pivot = stack->head->content;
-// 	head_current = stack->head;
-// 	tail_current = stack->head->previous;
-// 	while (stack)
-// 	{
-// 		while (head_current->content < pivot)
-// 			head_current = head_current->next;
-// 		while (tail_current->content > pivot)
-// 			tail_current = tail_current->previous;
-// 		if (head_current->index > tail_current->index)
-// 			break ;
-// 		// not swapping on original stack:
-// 		swap = head_current->content;
-// 		head_current->content = tail_current->content;
-// 		tail_current->content = swap;
-// 	}
-// 	return (stack);
-// }
--- push-swap/stack_operations.c
+++ push-swap/stack_operations.c
@@ -6,7 +6,7 @@
 /*   By: ncruz-ne <ncruz-ne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/07 19:57:59 by ncruz-ne          #+#    #+#             */
-/*   Updated: 2025/09/08 19:33:11 by ncruz-ne         ###   ########.fr       */
+/*   Updated: 2025/09/10 00:39:48 by ncruz-ne         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -31,3 +31,68 @@ t_stack	*create_empty_stack(void)
 // do I need to consider saving the content from OG stack first?
 // and freeing OG stack?
 // always check if stack->size == 0 before doing operations ()
+
+
+// function for rank?????
+// inversions = rank - index; goal is 0.
+// sorted => SUM(inversions). goal is 0.
+
+// bucket_sort + radix_sort ?
+// REQUIRES TESTING:
+// void	ft_mk_lst_rank(t_stack *stack)
+// {
+// 	t_nodes	nodes;
+// 	int		lsd;
+// 	int		bucket;
+// 	int		div;
+
+// 	nodes.previous = NULL;
+// 	nodes.current = NULL;
+// 	div = find_radix_max_div(stack);
+// 	while (bucket >= -9)
+// 	{
+// 		bucket = 9;
+// 		while (stack)
+// 		{
+// 			lsd = (stack->head->content) % div;
+// 			if (lsd == bucket)
+// 			{
+// 			}
+// 			stack->head = stack->head->next;
+// 			if (stack->head->index == 0)
+// 				break ;
+// 		}
+// 		bucket--;
+// 	}
+// }
+
+// quick_sort()
+// Hoare's partition
+// https://www.geeksforgeeks.org/dsa/hoare-s-partition-algorithm/
+// REQUIRES TESTING:
+// t_stack	*hoares_partition(t_stack *stack)
+// {
+// 	int			pivot;
+// 	int			swap;
+// 	t_circlist	*head_current;
+// 	t_circlist	*tail_current;
+// 	// t_stack		*cp_stack;
+
+// 	pivot = stack->head->content;
+// 	head_current = stack->head;
+// 	tail_current = stack->head->previous;
+// 	while (stack)
+// 	{
+// 		while (head_current->content < pivot)
+// 			head_current = head_current->next;
+// 		while (tail_current->content > pivot)
+// 			tail_current = tail_current->previous;
+// 		if (head_current->index > tail_current->index)
+// 			break ;
+// 		// not swapping on original stack:
+// 		swap = head_current->content;
+// 		head_current->content = tail_current->content;
+// 		tail_current->content = swap;
+// 	}
+// 	return (stack);
+// }
